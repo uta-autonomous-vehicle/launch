18f16a01b3d49eea3156c754abe2602f0b904c9c
Harshilkumar Patel <harshil912@gmail.com>
2019-09-11 15:57:04 -0500

    dir updates

--- racecar.sh
+++ racecar.sh
@@ -6,5 +6,5 @@ roslaunch racecar teleop.launch & \
 sleep 2 && roslaunch zed_wrapper zed.launch & \
 sleep 2 && roslaunch urg_node urg_lidar.launch & \
 sleep 2 && rqt & \
-sleep 2 && rviz -d ~/Documents/racecar/rviz/zed.rviz & \
-sleep 2 && rviz -d ~/Documents/racecar/rviz/lidar.rviz
+sleep 2 && rviz -d rviz/zed.rviz & \
+sleep 2 && rviz -d rviz/lidar.rviz
