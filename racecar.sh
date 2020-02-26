fbb6c0b46f8cd216d21784939f1c761ef7ca87fb
Harshilkumar Patel <harshil912@gmail.com>
2020-02-26 08:58:58 -0600

    adding local dir

--- racecar.sh
+++ racecar.sh
@@ -2,14 +2,15 @@
 
 # trap "kill 0" SIGINT
 
-workon racecar
+# workon racecar
+# echo "" > kill.txt
+
 source ~/racecar-ws/devel/setup.bash
 
-# echo "" > kill.txt
 
 roslaunch racecar teleop.launch  & \
 sleep 2 && roslaunch zed_wrapper zed.launch &  \
-sleep 2 && roslaunch urg_node urg_lidar.launch & \
+# sleep 2 && roslaunch urg_node urg_lidar.launch & \
 sleep 2 && rqt & \
 sleep 2 && rviz -d rviz/zed.rviz & \
 sleep 2 && rviz -d rviz/lidar.rviz
