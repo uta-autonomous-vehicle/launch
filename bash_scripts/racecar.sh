b41524486b4e798a1956e95746f7fe2cefa75fc2
Harshilkumar Patel <harshil912@gmail.com>
2020-09-17 20:42:01 -0500

    prep for recording relevant data

--- bash_scripts/racecar.sh
+++ bash_scripts/racecar.sh
@@ -7,16 +7,17 @@ source ~/racecar-ws/devel/setup.bash
 
 . mount.sh
 roslaunch racecar teleop.launch \
-& sleep 5 && roslaunch --wait zed_wrapper zed.launch 
+& sleep 5 && roslaunch --wait zed_wrapper zed.launch \
+& sleep 10 && roslaunch --wait --screen -v urg_node urg_lidar.launch
+# & sleep 10 && roslaunch --wait --screen -v uta-racecar racecar_launch.launch 
+
 # & sleep 15 && rqt \
 # & sleep 20 && rviz -d ../rviz/zed.rviz \
 # & sleep 25 && rviz -d ../rviz/lidar.rviz
-# & sleep 10 && roslaunch --wait --screen -v urg_node urg_lidar.launch \
 # & sleep 5 && roslaunch --wait --screen -v usb_cam usb_cam-test.launch \
 # & sleep 10 && roslaunch --wait --screen -v rosbridge_server rosbridge_websocket.launch
 
 
 # & sudo xboxdrv -s --quiet --priority normal \ # run this on a seperate terminal. running this beside the roslaunch freezes the system
-# & sleep 10 && roslaunch --wait --screen -v uta-racecar racecar_launch.launch \
 
 # visualize data
