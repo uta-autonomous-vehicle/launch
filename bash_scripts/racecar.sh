8530c8a45603038dbde3fd77fe95d09ef33b727f
Harshilkumar Patel <harshil912@gmail.com>
2020-11-12 15:41:30 -0600

    adding rviz launch config

--- bash_scripts/racecar.sh
+++ bash_scripts/racecar.sh
@@ -3,18 +3,20 @@
 # trap "kill 0" SIGINT
 
 # workon racecar
-source ~/racecar-ws/devel/setup.bash
+source /opt/ros/kinetic/setup.bash
+source ~/racecar-workspace/devel/setup.bash
 
 . mount.sh
 roslaunch racecar teleop.launch \
-& sleep 5 && roslaunch --wait zed_wrapper zed.launch \
-& sleep 10 && roslaunch --wait --screen -v urg_node urg_lidar.launch
-# & sleep 10 && roslaunch --wait --screen -v uta-racecar racecar_launch.launch 
+& sleep 5 && roslaunch --wait zed_wrapper zed.launch resolution:=3 frame_rate:=60 
+# & sleep 5 && roslaunch --wait --screen -v usb_cam usb_cam-test.launch 
 
+# & sleep 10 && roslaunch --wait --screen -v uta-racecar racecar_save_vision.launch 
+# & sleep 10 && roslaunch --wait --screen -v uta-racecar racecar_launch.launch 
+# & sleep 10 && roslaunch --wait --screen -v urg_node urg_lidar.launch \
 # & sleep 15 && rqt \
 # & sleep 20 && rviz -d ../rviz/zed.rviz \
 # & sleep 25 && rviz -d ../rviz/lidar.rviz
-# & sleep 5 && roslaunch --wait --screen -v usb_cam usb_cam-test.launch \
 # & sleep 10 && roslaunch --wait --screen -v rosbridge_server rosbridge_websocket.launch
 
 
---
+++ bash_scripts/record.sh
@@ -0,0 +1,2 @@
+# rosbag record -o vision_ /vesc/joy /scan /tf /vesc/low_level/ackermann_cmd_mux/output /vesc/odom /zed/depth/depth_registered /zed/disparity/disparity_image /zed/odom /zed/path_odom /zed/rgb/image_raw_color 
+rosbag record -o vision_all_ -a
